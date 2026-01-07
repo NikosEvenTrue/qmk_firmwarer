31de2899c868b04983cef2ef6d21d90798a7257a
Niko Seven <nikita.xakimov.77@gmail.com>
2026-01-07 16:30:45 +0300

    all russians symbols

--- keyboards/tweetydabird/lotus58/keymaps/unicode/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/unicode/keymap.c
@@ -178,11 +178,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+// Defines the key combination for the combo
+const uint16_t PROGMEM b_softsign_combo[] = {KC_COMM, KC_M, COMBO_END};
+
+// Defines the combos themselves
+combo_t key_combos[] = {
+    COMBO(b_softsign_combo, KC_RBRC),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-    QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,        XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,        XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC,
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,        XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                       KC_LGUI, KC_SPC,  MO(1),   KC_LCTL,                 KC_LALT, MO(2),   KC_ENT,  XXXXXXX
     ),
@@ -196,19 +204,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [2] = LAYOUT(
-          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, _______,        _______, KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
-        _______, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                          _______, KC_LEFT, KC_RGHT, KC_DOWN, KC_UP,   _______,
-        KC_LSFT, _______, _______, _______, _______, _______, _______,        _______, _______, KC_HOME, KC_END,  KC_PGDN, KC_PGUP, KC_RSFT,
-                                   _______, KC_SPC, MO(3),   KC_LCTL,         KC_LALT, _______, KC_ENT, _______
+          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, XXXXXXX,        XXXXXXX, KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          KC_DEL, KC_LEFT, KC_RGHT, KC_DOWN, KC_UP,   XXXXXXX,
+        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, KC_HOME, KC_END,  KC_PGDN, KC_PGUP, KC_RSFT,
+                                   XXXXXXX, KC_SPC, MO(3),   KC_LCTL,         KC_LALT, XXXXXXX, KC_ENT, XXXXXXX
     ),
 
     [3] = LAYOUT(
-        QK_BOOT, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, QK_BOOT,
-        _______, _______, _______, _______, _______, _______,                          _______, _______, _______,  _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______,
-                                   _______, _______, _______, _______,        _______, _______, _______, _______ 
+        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX 
     )
 };
 
