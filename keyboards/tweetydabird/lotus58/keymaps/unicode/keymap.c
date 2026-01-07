c60d441a0350241bba07154e3fb3ea1841479075
Niko Seven <nikita.xakimov.77@gmail.com>
2026-01-07 16:50:53 +0300

    change language

--- keyboards/tweetydabird/lotus58/keymaps/unicode/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/unicode/keymap.c
@@ -34,6 +34,7 @@ enum custom_keycodes {
     NIKO_UC_PLUS,              // U+002B -> +
     NIKO_UC_LPRN,              // U+0028 -> (
     NIKO_UC_RPRN,              // U+0029 -> )
+    NIKO_CHNG_LNG,             // LGUI + Space (Change Language)
 };
 
 void tap_numpad_key(uint8_t digit) {
@@ -173,6 +174,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             case NIKO_UC_RPRN:
                 send_alt_numpad_code(41);
                 return false;
+            case NIKO_CHNG_LNG:
+                register_code(KC_LGUI);
+                tap_code(KC_SPC);
+                unregister_code(KC_LGUI);
+                return false;
         }
     }
     return true;
@@ -188,15 +194,15 @@ combo_t key_combos[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-    KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,        XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC,
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,        XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,        XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      KC_LGUI, KC_SPC,  MO(1),   KC_LCTL,                 KC_LALT, MO(2),   KC_ENT,  XXXXXXX
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,        XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                   KC_LGUI, KC_SPC,   MO(1),   KC_LCTL,        KC_BSPC, MO(2),   KC_LALT,  NIKO_CHNG_LNG
     ),
 
     [1] = LAYOUT(
-        XXXXXXX, XXXXXXX,      XXXXXXX,      NIKO_UC_NUMO, XXXXXXX,      XXXXXXX,      XXXXXXX,                XXXXXXX, XXXXXXX,       XXXXXXX,       XXXXXXX,       NIKO_UC_LCBR,  NIKO_UC_RCBR,  XXXXXXX,
+        XXXXXXX, XXXXXXX,      XXXXXXX,      NIKO_UC_NUMO, XXXXXXX,      XXXXXXX,      XXXXXXX,                XXXXXXX, XXXXXXX,       XXXXXXX,       XXXXXXX,       XXXXXXX,  XXXXXXX,  XXXXXXX,
         XXXXXXX, NIKO_UC_EXLM, NIKO_UC_AT,   NIKO_UC_HASH, NIKO_UC_DLR,  NIKO_UC_PERC,                                  XXXXXXX,       NIKO_UC_LPRN,  NIKO_UC_RPRN,  NIKO_UC_LBRC,  NIKO_UC_RBRC,  XXXXXXX,
         XXXXXXX, NIKO_UC_PLUS, NIKO_UC_MINS, NIKO_UC_ASTR, NIKO_UC_SLSH, NIKO_UC_BSLS,                                  NIKO_UC_GRV,   NIKO_UC_QUOT,  NIKO_UC_DQUO,  NIKO_UC_COLN,  NIKO_UC_SCLN,  XXXXXXX,
         XXXXXXX, XXXXXXX,      NIKO_UC_PIPE, NIKO_UC_AMPR, NIKO_UC_UNDS, XXXXXXX,      XXXXXXX,        XXXXXXX,       NIKO_UC_LCBR,  NIKO_UC_RCBR,  NIKO_UC_COMM,    NIKO_UC_DOT,   NIKO_UC_QUES,  XXXXXXX,
@@ -206,7 +212,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT(
           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, XXXXXXX,        XXXXXXX, KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          KC_DEL, KC_LEFT, KC_RGHT, KC_DOWN, KC_UP,   XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL, XXXXXXX, XXXXXXX,                          XXXXXXX, KC_LEFT, KC_RGHT, KC_DOWN, KC_UP,   XXXXXXX,
         KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, KC_HOME, KC_END,  KC_PGDN, KC_PGUP, KC_RSFT,
                                    XXXXXXX, KC_SPC, MO(3),   KC_LCTL,         KC_LALT, XXXXXXX, KC_ENT, XXXXXXX
     ),
